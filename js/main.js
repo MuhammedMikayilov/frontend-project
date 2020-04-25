7c2ef5eb9973efab29152db3239f847f563f7809
MuhammedMikayilov <mahammadsm@code.edu.az>
2020-04-25 09:57:45 +0400

    intro section responsive is completed

--- js/main.js
+++ js/main.js
@@ -65,15 +65,15 @@ $(document).ready(function () {
   });
 
   //   responsive
-//   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-//   function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
+  //   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  //   function myFunction() {
+  //     var x = document.getElementById("myTopnav");
+  //     if (x.className === "topnav") {
+  //       x.className += " responsive";
+  //     } else {
+  //       x.className = "topnav";
+  //     }
+  //   }
 
   //   navbar end
 });
