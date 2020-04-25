a0ff0fd5c6af742d105f5895066872268ab2a2a7
MuhammedMikayilov <mahammadsm@code.edu.az>
2020-04-25 05:52:21 +0400

    Almost ready popup

--- js/main.js
+++ js/main.js
@@ -7,15 +7,64 @@ $(document).ready(function () {
     }
   });
 
-  $("nav .icon-navbar .searchBtn").click(function () {
-    $(".popup").css({
-      tansition: "all .4s",
-      display: "flex",
-    });
+  $("nav .icon-navbar .searchBtn").click(function (e) {
+    e.preventDefault();
+
+    $(".pop-up").fadeIn().removeClass("active");
+    closePop();
+    setCloseKey();
+    closePopUp();
   });
 
   //   popup
-  var pop = $(".popup");
+  var pop = $(".pop-up");
+  var closeBtn = $(".icon-pop");
+
+  //   $(".popup").click(function () {
+  //     $("#email-popup").show("fast");
+  //   });
+  //   $(".phone").click(function () {
+  //     $("#phone-popup").show();
+  //   });
+
+  $(".popup").click(function () {
+    // $("#phone-popup").hide("fast");
+  });
+
+  pop.click(function (p) {
+    // if (p.target.hasClass("popup")) {
+    //   pop.css("display", "none");
+    // }
+    // $(".search").not()
+  });
+  //   functions
+  function closePop() {
+    closeBtn.click(function () {
+      //   pop.css("display", "none");
+      pop.fadeOut().removeClass("active");
+    });
+  }
+
+  function setCloseKey() {
+    $("body").keydown(function (e) {
+      if (e.which == 27) {
+        // pop.css("display", "none");
+        pop.fadeOut().removeClass("active");
+      }
+    });
+  }
 
   //   navbar end
+  var popup = document.querySelector(".pop-up");
+
+  popup.addEventListener("click", function (p) {
+    if (!p.target.classList.contains("pop-up")) {
+      //   popup.style.display = "none";
+      //   pop.css("display", "none");
+      console.log("yes");
+    }
+    // console.log("hello");
+  });
 });
+
+function closePopUp() {}
