9edcbfc9c54fca78c55ce44456a1cd0070603702
MuhammedMikayilov <mahammadsm@code.edu.az>
2020-04-25 08:39:03 +0400

    Responsive navbar ready

--- js/main.js
+++ js/main.js
@@ -64,5 +64,16 @@ $(document).ready(function () {
     $(".sidebar").removeClass("open-side");
   });
 
+  //   responsive
+//   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+//   function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+
   //   navbar end
 });
