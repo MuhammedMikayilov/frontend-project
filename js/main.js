090d83f3f37679d81341bbbb2bda4ad4e5ac096d
MuhammedMikayilov <mahammadsm@code.edu.az>
2020-04-25 04:16:33 +0400

    completed search form into popup

--- js/main.js
+++ js/main.js
@@ -6,17 +6,16 @@ $(document).ready(function () {
       $(".navbar .nav-menu").removeClass("bg-color-white");
     }
   });
-  //   navbar end
 
-  // venobox
-  $(".venobox_custom").venobox({
-    framewidth: "400px", // default: ''
-    frameheight: "300px", // default: ''
-    border: "10px", // default: '0'
-    bgcolor: "#5dff5e", // default: '#fff'
-    titleattr: "data-title", // default: 'title'
-    numeratio: true, // default: false
-    infinigall: true, // default: false
-    share: ["facebook", "twitter", "download"], // default: []
+  $("nav .icon-navbar .searchBtn").click(function () {
+    $(".popup").css({
+      tansition: "all .4s",
+      display: "flex",
+    });
   });
+
+  //   popup
+  var pop = $(".popup");
+
+  //   navbar end
 });
